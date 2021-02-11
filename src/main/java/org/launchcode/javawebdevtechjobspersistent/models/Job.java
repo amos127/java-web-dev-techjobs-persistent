3c145469edcd9b3d8d3409cdf42e38c9a1da1bb9
amy sanders <amysanders89@gmail.com>
2021-02-10 18:37:19 -0600

    completed assignment

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
@@ -1,7 +1,6 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
